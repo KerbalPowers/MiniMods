c9375bafd08604bf82e7d12c7923b91bcffc4e19
Spartwo <sparthesecond@gmail.com>
2024-10-29 19:19:22 +0000

    Source

---
+++ Source/DepletionMod/Depletion/ModuleBatteryDrain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using static PartModule;
+
+namespace Depletion
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class BatteryDrain : PartModule
+    {
+        [KSPField] public float percentFull = 100;
+
+        public override void OnStart(StartState state)
+        {
+            if (part.Resources.Contains("ElectricCharge"))
+            {
+                // Find the electric charge resource in the part
+                var electricChargeResource = part.Resources.Get("ElectricCharge");
+                float max = (float)electricChargeResource.maxAmount;
+                float current = (float)electricChargeResource.amount;
+
+                //Calculate how much to drain to reach the amount
+                float demand = (max - (max * (percentFull / 100))) - (max-current);
+                //Debug.Log("[Depletion] Demand: " + demand);
+
+                if (demand > 0)
+                {
+                    // Set the amount of the electric charge by subtracting what we don't want
+                    Debug.Log("[Depletion] Draining to " + percentFull + "%");
+                    part.RequestResource("ElectricCharge", demand, ResourceFlowMode.NO_FLOW);
+                }
+            }
+
+        }
+    }
+}
---
+++ Source/TorqueMod/Torquer/ModuleTorqueCap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static PartModule;
+
+namespace Torquer
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class ModuleTorqueCap : PartModule
+    {
+        [KSPField] public float torquePerTon = 1;
+
+        private float totalTorque = 0;
+        private float torquePercent;
+
+        // The array list to store the found modules
+        private ArrayList foundModules = new ArrayList();
+        public override void OnStart(StartState state)
+        {
+            // Iterate through each part on the vessel
+            foreach (Part part in vessel.Parts)
+            {
+                if (part.Modules.Contains("ModuleReactionWheel"))
+                {
+                    ModuleReactionWheel torque = part.FindModuleImplementing<ModuleReactionWheel>();
+                    totalTorque += (torque.PitchTorque+torque.YawTorque+torque.RollTorque)/3;
+                    foundModules.Add(torque);
+                }
+            }
+
+            torquePercent = ((part.vessel.GetTotalMass() * torquePerTon) / totalTorque)*100;
+
+
+            // Iterate through each part on the vessel
+            foreach (ModuleReactionWheel torque in foundModules)
+            {
+                torque.authorityLimiter = Mathf.Min(100f, torquePercent);
+            }
+          
+        }
+    }
+}
