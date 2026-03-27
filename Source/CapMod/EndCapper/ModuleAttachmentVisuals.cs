4a6391b508f8a676e47f0eed54a0c9b3a1088d8b
Halban <30965946+Halbann@users.noreply.github.com>
2026-03-27 12:55:22 +0000

    Code cleanup

--- Source/CapMod/EndCapper/ModuleAttachmentVisuals.cs
+++ Source/CapMod/EndCapper/ModuleAttachmentVisuals.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace AttachmentVisuals
@@ -10,10 +11,11 @@ namespace AttachmentVisuals
         // attachment node (only when advanced tweakables is enabled). Will
         // need implement IConfigNode on NodeData so state can be saved rather than inferred.
 
+        // todo: serialise per-node state so that they never change during flight.
+
         public class NodeVisual
         {
             public AttachNode attachNode;
-
             public List<Transform> showWhenAttached;
             public List<Transform> showWhenFree;
 
@@ -33,33 +35,22 @@ namespace AttachmentVisuals
                     return false;
                 }
 
-                bool valid = false;
-                valid = valid || TryLoadlist(ref showWhenAttached, part, configNode, "showWhenAttached");
-                valid = valid || TryLoadlist(ref showWhenFree, part, configNode, "showWhenFree");
-
-                return valid;
+                return TryLoadList(ref showWhenAttached, part, configNode, "showWhenAttached")
+                    | TryLoadList(ref showWhenFree, part, configNode, "showWhenFree");
             }
 
-            private bool TryLoadlist(ref List<Transform> list, Part part, ConfigNode configNode, string listName)
+            private bool TryLoadList(ref List<Transform> list, Part part, ConfigNode configNode, string listName)
             {
                 string listString = null;
-                if (!configNode.TryGetValue(listName, ref listString))
+                if (!configNode.TryGetValue(listName, ref listString) || string.IsNullOrWhiteSpace(listString))
                     return false;
 
-                if (string.IsNullOrWhiteSpace(listString))
-                    return false;
-
-                string[] transformList = listString.Split(',');
-
-                if (transformList.Length < 1)
-                    return false;
-
-                foreach (string transformName in transformList)
+                foreach (string transformName in listString.Split(','))
                 {
                     Transform t = part.FindModelTransform(transformName.Trim());
 
                     if (t != null)
-                        (list ?? (list = new List<Transform>())).Add(t);
+                        (list ??= new List<Transform>()).Add(t);
                     else
                         Debug.LogError($"[ModuleAttachmentVisuals]: Could not find transform '{transformName}' on {part.name}");
                 }
@@ -67,46 +58,33 @@ namespace AttachmentVisuals
                 return true;
             }
 
-            public void ApplyVisiblity(bool attached)
+            public void UpdateVisibility()
             {
-                ApplyList(showWhenAttached, attached);
-                ApplyList(showWhenFree, !attached);
+                if (attachNode != null)
+                    ApplyVisibility(attachNode.attachedPart != null);
             }
 
-            private void ApplyList(List<Transform> list, bool show)
+            public void ApplyVisibility(bool attached)
             {
-                if (list == null)
-                    return;
-
-                foreach (var t in list)
-                {
-                    if (t == null)
-                        continue;
-
-                    t.gameObject.SetActive(show);
-                }
+                showWhenAttached?.ForEach(t => t?.gameObject.SetActive(attached));
+                showWhenFree?.ForEach(t => t?.gameObject.SetActive(!attached));
             }
         }
 
-        [SerializeField]
-        private string[] nodeVisualConfigs;
-
-        [NonSerialized]
-        public List<NodeVisual> nodeVisuals;
-
+        [NonSerialized] public List<NodeVisual> nodeVisuals;
+        [SerializeField] private string[] nodeVisualConfigs;
         private HashSet<Part> directChildren;
 
         // --- Lifecycle ---
 
         public override void OnLoad(ConfigNode node)
         {
+            if (nodeVisualConfigs != null)
+                return;
+
             ConfigNode[] nodesCfg = node.GetNodes("NODEVISUAL");
             if (nodesCfg.Length > 0)
-            {
-                nodeVisualConfigs = new string[nodesCfg.Length];
-                for (int i = 0; i < nodesCfg.Length; i++)
-                    nodeVisualConfigs[i] = nodesCfg[i].ToString();
-            }
+                nodeVisualConfigs = nodesCfg.Select(n => n.ToString()).ToArray();
         }
 
         public override void OnStart(StartState state)
@@ -118,10 +96,10 @@ namespace AttachmentVisuals
             {
                 try
                 {
-                    NodeVisual newNode = new NodeVisual();
+                    var newNode = new NodeVisual();
 
                     if (newNode.Load(part, ConfigNode.Parse(nodeVisualConfigs[i]).GetNode("NODEVISUAL")))
-                        (nodeVisuals ?? (nodeVisuals = new List<NodeVisual>())).Add(newNode);
+                        (nodeVisuals ??= new List<NodeVisual>()).Add(newNode);
                 }
                 catch { }
             }
@@ -133,7 +111,7 @@ namespace AttachmentVisuals
             if (HighLogic.LoadedSceneIsEditor)
             {
                 GameEvents.onEditorPartEvent.Add(OnEditorEvent);
-                CacheInitialChildren();
+                directChildren = part.children.ToHashSet();
             }
 
             UpdateVisuals();
@@ -141,70 +119,25 @@ namespace AttachmentVisuals
 
         public void OnDestroy()
         {
-            if (nodeVisuals == null)
-                return;
-
-            if (HighLogic.LoadedSceneIsEditor)
+            if (nodeVisuals != null && HighLogic.LoadedSceneIsEditor)
                 GameEvents.onEditorPartEvent.Remove(OnEditorEvent);
         }
 
         // --- Functions ---
 
-        private void UpdateVisuals()
-        {
-            if (nodeVisuals == null)
-                return;
-
-            foreach (var nodeData in nodeVisuals)
-            {
-                if (nodeData.attachNode == null)
-                    continue;
-
-                nodeData.ApplyVisiblity(nodeData.attachNode.attachedPart != null);
-            }
-        }
+        private void UpdateVisuals() => nodeVisuals?.ForEach(n => n.UpdateVisibility());
 
         private void OnEditorEvent(ConstructionEventType evt, Part p)
         {
-            // Only care about attach/detach
             if (evt != ConstructionEventType.PartAttached && evt != ConstructionEventType.PartDetached)
                 return;
 
-            // If the event is on this part
-            if (part == p)
+            if (p == part
+                || (evt == ConstructionEventType.PartAttached && p.parent == part && directChildren.Add(p))
+                || (evt == ConstructionEventType.PartDetached && directChildren.Remove(p)))
             {
                 UpdateVisuals();
-                return;
             }
-
-            // Only care about events involving this parts direct relatives
-            switch (evt)
-            {
-                case ConstructionEventType.PartAttached:
-                    if (p.parent == part)
-                    {
-                        directChildren.Add(p);
-                        UpdateVisuals();
-                    }
-                    break;
-
-                case ConstructionEventType.PartDetached:
-                    if (directChildren.Contains(p))
-                    {
-                        directChildren.Remove(p);
-                        UpdateVisuals();
-                    }
-                    break;
-            }
-        }
-
-        private void CacheInitialChildren()
-        {
-            // Populate children at start to track later
-            directChildren = new HashSet<Part>();
-
-            foreach (var child in part.children)
-                directChildren.Add(child);
         }
     }
 }
